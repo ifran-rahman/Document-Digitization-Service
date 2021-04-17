73cb76c53da85f9d645ee79b7163e9e8054716a4
Moshfique <moshfique.uddin@northsouth.edu>
2021-04-17 19:01:55 +0600

    Working Android Navigation Bar

---
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/FolderFragment.java
@@ -0,0 +1,17 @@
+package com.example.documentdigitizationservice;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FolderFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_folder, container, false);
+    }
+}
--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.documentdigitizationservice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.home_activity:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.folder_activity:
+                            selectedFragment = new FolderFragment();
+                            break;
+                        case R.id.notification_activity:
+                            selectedFragment = new NotificationsFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
