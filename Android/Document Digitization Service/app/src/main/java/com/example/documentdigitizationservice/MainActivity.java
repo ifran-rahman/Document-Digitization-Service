28b098b1818bcb662193420b42036b92a137cdcb
ifran <ifran.nijhum@northsouth.edu>
2021-04-18 01:40:39 +0600

    deleted unnecessary line

--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.documentdigitizationservice;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
