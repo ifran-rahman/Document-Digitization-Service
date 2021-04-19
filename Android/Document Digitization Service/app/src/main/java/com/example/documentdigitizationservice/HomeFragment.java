7cd25dec616265668132e66f32f4ad62f43234a7
ifran <ifran.nijhum@northsouth.edu>
2021-04-19 17:46:39 +0600

    added complete recycler view to home

--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/HomeFragment.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/HomeFragment.java
@@ -1,4 +1,5 @@
 package com.example.documentdigitizationservice;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,49 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    private ArrayList<String> mUserNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mUserRoles = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        mImageUrls.add("https://scontent.fzyl1-1.fna.fbcdn.net/v/t1.6435-9/158900688_2019518771524579_9061371679276739948_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=SLG-4hOx6-8AX_hE9a7&_nc_ht=scontent.fzyl1-1.fna&oh=4c5948625d929be61edf1bb95a055904&oe=60A02D13");
+        mUserNames.add("Anan Ghosh");
+        mUserRoles.add("Developer");
+        mImageUrls.add("https://scontent.fzyl1-1.fna.fbcdn.net/v/t1.6435-9/43322618_486658725170587_5381512967265189888_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=DMd56GrqcHgAX_BSJT_&_nc_ht=scontent.fzyl1-1.fna&oh=98f608adca6118c98551683ba9a47c43&oe=609F54E7");
+        mUserNames.add("Moshfique Uddin");
+        mUserRoles.add("Developer");
+        mImageUrls.add("https://scontent.fzyl1-1.fna.fbcdn.net/v/t1.6435-9/29365773_10209418393335535_3763052488496775168_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=174925&_nc_ohc=jf-4vU67vJwAX-S6KVt&_nc_ht=scontent.fzyl1-1.fna&oh=72a9138197f4b3d77729980d475b91c8&oe=609FCB6A");
+        mUserNames.add("Hasibul Hassan");
+        mUserRoles.add("Developer");
+        mImageUrls.add("https://scontent.fzyl1-1.fna.fbcdn.net/v/t1.6435-9/94614491_2624999207779215_4015548479047729152_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=174925&_nc_ohc=ma3pKkLMHUEAX83OMkI&_nc_ht=scontent.fzyl1-1.fna&oh=c87d347089be0ebe7f119c6ea1f2a99d&oe=60A11804");
+        mUserNames.add("Ifran Rahman Nijhum");
+        mUserRoles.add("Developer");
+
+        RecyclerView recyclerView =  view.findViewById(R.id.recyler_view);
+
+        HomeViewAdapter adapter = new HomeViewAdapter(mImageUrls, mUserNames, mUserRoles);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
     }
+
+
+
 }
---
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/HomeViewAdapter.java
@@ -0,0 +1,91 @@
+package com.example.documentdigitizationservice;
+
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.documentdigitizationservice.R;
+
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class HomeViewAdapter extends RecyclerView.Adapter<HomeViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerviewAdapter";
+
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mUserNames = new ArrayList<>();
+    private ArrayList<String> mUserRoles = new ArrayList<>();
+    private Context mContext;
+
+    public HomeViewAdapter(ArrayList<String> mImages, ArrayList<String> mImagenames, ArrayList<String> mUserRoles) {
+        this.mImages = mImages;
+        this.mUserNames = mImagenames;
+        this.mUserRoles = mUserRoles;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_item, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int position) {
+
+        Glide.with( viewHolder.image.getContext())
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(viewHolder.image);
+        Log.d(TAG, "onBindviewholder");
+
+        // Set the name of the 'user'
+        ((ViewHolder)viewHolder).username.setText(mUserNames.get(position));
+        ((ViewHolder)viewHolder).userrole.setText(mUserRoles.get(position));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mUserNames.get(position));
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUserNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView image;
+        TextView username;
+        TextView userrole;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            username = itemView.findViewById(R.id.username);
+            userrole = itemView.findViewById(R.id.userrole);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+
+
+}
