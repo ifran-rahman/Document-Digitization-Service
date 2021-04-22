92d401b9834a0cf6128f05bd73aa62f7bc58c8a6
ifran <ifran.nijhum@northsouth.edu>
2021-04-22 11:02:04 +0600

    completed notification panel ui

--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/NotificationViewAdapter.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/NotificationViewAdapter.java
@@ -8,36 +8,29 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class NotificationViewAdapter extends RecyclerView.Adapter<NotificationViewAdapter.ViewHolder>{
 
     private static final String TAG = "NotificationViewAdapter";
 
-    private ArrayList<String> mUserNames = new ArrayList<>();
-    private ArrayList<String> mUserRoles = new ArrayList<>();
+    private ArrayList<String> mNotificationNames;
+    private ArrayList<String> mNotificationDetails;
     private Context mContext;
 
-    public NotificationViewAdapter(ArrayList<String> mImagenames, ArrayList<String> mUserRoles, Context mContext) {
-
-        this.mUserNames = mImagenames;
-        this.mUserRoles = mUserRoles;
+    public NotificationViewAdapter(ArrayList<String> mNotificationNames, ArrayList<String> mNotificationDetails, Context mContext) {
+        this.mNotificationNames = mNotificationNames;
+        this.mNotificationDetails = mNotificationDetails;
         this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_item, parent, false);
         ViewHolder vh = new ViewHolder(view);
         return vh;
     }
@@ -45,21 +38,21 @@ public class NotificationViewAdapter extends RecyclerView.Adapter<NotificationVi
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
 
-        ((ViewHolder)viewHolder).NotificationName.setText(mUserNames.get(position));
-        ((ViewHolder)viewHolder).NotificationDetails.setText(mUserRoles.get(position));
+        viewHolder.NotificationName.setText(mNotificationNames.get(position));
+        viewHolder.NotificationDetails.setText(mNotificationDetails.get(position));
 
         viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: clicked on: " + mUserNames.get(position));
-                Toast.makeText(mContext, mUserNames.get(position), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: clicked on: " + mNotificationNames.get(position));
+                Toast.makeText(mContext, mNotificationNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return mUserNames.size();
+        return mNotificationNames.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -70,7 +63,7 @@ public class NotificationViewAdapter extends RecyclerView.Adapter<NotificationVi
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             NotificationName = itemView.findViewById(R.id.NotificationName);
-            NotificationDetails = itemView.findViewById(R.id.NotificationName);
+            NotificationDetails = itemView.findViewById(R.id.NotificationDetails);
             parentLayout = itemView.findViewById(R.id.notification_item_layout);
         }
     }
--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/NotificationsFragment.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/NotificationsFragment.java
@@ -1,4 +1,5 @@
 package com.example.documentdigitizationservice;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,52 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
+
+    private ArrayList<String> mNotificationNames = new ArrayList<>();
+    private ArrayList<String> mNotificationDetails = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        initBitmaps();
+        setLayout(view);
+        return view;
     }
+
+    public void initBitmaps() {
+        mNotificationNames.add("Welcome");
+        mNotificationDetails.add("Welcome to Plan International");
+        mNotificationNames.add("New account");
+        mNotificationDetails.add("This is your new account. Your password: 1234. Change it ass soon as possible");
+        mNotificationNames.add("Password change");
+        mNotificationDetails.add("Your request for changing password is accepted");
+        mNotificationNames.add("Hasibul Hassan");
+        mNotificationDetails.add("Kire mama ki khobor?");
+
+    }
+
+    public void setLayout(View view) {
+
+        RecyclerView recyclerView = view.findViewById(R.id.notification_recycler_view);
+
+        Context context = getActivity();
+        NotificationViewAdapter adapter = new NotificationViewAdapter(mNotificationNames, mNotificationDetails, context);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+    }
+
 }
