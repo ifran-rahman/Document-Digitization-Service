d7c1043eec7c07dedd101b39dc1b50ab7e5f1bf6
ifran <ifran.nijhum@northsouth.edu>
2021-04-24 11:36:05 +0600

    added private repo recyclerview

---
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/PrivateRepoAdapter.java
@@ -0,0 +1,76 @@
+package com.example.documentdigitizationservice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class PrivateRepoAdapter extends RecyclerView.Adapter {
+    private static final String TAG = "PrivateRepo dapter";
+
+    private ArrayList<String> mFolderNames;
+    private Context mContext;
+
+
+    public PrivateRepoAdapter(ArrayList<String> mFolderNames, Context mContext) {
+        this.mFolderNames = mFolderNames;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public PrivateRepoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.private_repo_item, parent, false);
+        PrivateRepoAdapter.ViewHolder vh = new PrivateRepoAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int position) {
+
+        ((PrivateRepoAdapter.ViewHolder)viewHolder).username.setText(mFolderNames.get(position));
+
+        ((ViewHolder) viewHolder).parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mFolderNames.get(position));
+                Toast.makeText(mContext, mFolderNames.get(position), Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(mContext, ProfileActivity.class);
+                //mContext.startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mFolderNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView username;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.folder_name);
+            parentLayout = itemView.findViewById(R.id.private_repo_item_layout);
+        }
+    }
+
+
+}
--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/PrivateRepoFragment.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/PrivateRepoFragment.java
@@ -1,17 +1,55 @@
 package com.example.documentdigitizationservice;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class FolderFragment extends Fragment {
+import java.util.ArrayList;
+
+public class PrivateRepoFragment extends Fragment {
+
+    private ArrayList<String> mFolderNames = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_folder, container, false);
+        View view =  inflater.inflate(R.layout.fragment_private_repo, container, false);
+
+        initBitmaps();
+        setLayout(view);
+        return view;
     }
+
+    public void initBitmaps(){
+        mFolderNames.add("New folder");
+        mFolderNames.add("Management folder");
+        mFolderNames.add("Employe salary");
+        mFolderNames.add("habi jabi");
+        mFolderNames.add("lsjfj");
+
+    }
+    public void setLayout(View view){
+
+        RecyclerView recyclerView =  view.findViewById(R.id.private_repo_recyler_view);
+        Context context = getActivity();
+        PrivateRepoAdapter adapter = new PrivateRepoAdapter(mFolderNames,context);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+    }
+
+
 }
