9ec4474f7bac44f7134119a43d9962996ca0a4c6
ifran <ifran.nijhum@northsouth.edu>
2021-05-04 00:23:07 +0600

    added authstate listener

--- Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
+++ Android/Document Digitization Service/app/src/main/java/com/example/documentdigitizationservice/MainActivity.java
@@ -14,12 +14,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mEmail,mPassword;
     Button mLoginBtn;
     FirebaseAuth fAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -33,42 +35,65 @@ public class MainActivity extends AppCompatActivity {
         mPassword = findViewById(R.id.password);
         mLoginBtn = findViewById(R.id.loginbutton);
 
-        if (fAuth.getCurrentUser() != null) {
-            startActivity(new Intent(getApplicationContext(), Home.class));
-            finish();
-        }
-
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
-                if (TextUtils.isEmpty(email)) {
-                    mEmail.setError("Email is required");
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    mPassword.setError("Password is required");
-                    return;
-                }
-                Log.v(TAG,email);
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.v(TAG,"Logged In");
-                            Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), Home.class));
-                        } else {
-                            Log.v(TAG,"Not Logged In");
-                            Toast.makeText(MainActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(getApplicationContext(), Home.class);
+                    FirebaseQueries firebaseQueries= FirebaseQueries.getInstance();
+                    firebaseQueries.setUID(user.getUid());
+                    startActivity(intent);
+                    finish();
+                }else {
+                    mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String email = mEmail.getText().toString().trim();
+                            String password = mPassword.getText().toString().trim();
+                            if (TextUtils.isEmpty(email)) {
+                                mEmail.setError("Email is required");
+                                return;
+                            }
+                            if (TextUtils.isEmpty(password)) {
+                                mPassword.setError("Password is required");
+                                return;
+                            }
+                            Log.v(TAG,email);
+
+                            fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.v(TAG,"Logged In");
+                                        Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), Home.class));
+                                    } else {
+                                        Log.v(TAG,"Not Logged In");
+                                        Toast.makeText(MainActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
                         }
-                    }
-                });
+                    });
 
+                }
             }
-        });
+        };
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fAuth.removeAuthStateListener(mAuthStateListener);
     }
 
 }
